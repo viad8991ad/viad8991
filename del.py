ba9b3d5ec15e13eb3b917d5fa6601c3648737c3a
vshevchenko <shevchenko@logema.org>
2024-02-26 18:13:58 +0300

    save

---
+++ del.py
@@ -0,0 +1,50 @@
+import json
+import os
+
+from PIL import Image
+
+
+def prepare_ch_json():
+    data = {}
+    for i in os.listdir("static/img/characters/"):
+        if i.startswith("Character"):
+            name = i.replace("Character_", "").replace("_Resize_Wish_500_500.webp", "")
+            data[name.replace("_", " ")] = {
+                "element": "hydro",
+                "ru": "Е Лань",
+                "href": os.path.join("/static/img/characters", i)
+            }
+
+    with open("data/characters.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False)
+
+    print("prepare_ch_json is done")
+
+
+def resize():
+    for i in os.listdir("static/img/characters/original"):
+        if i.startswith("Character"):
+            img = Image.open("static/img/characters/original/" + i)
+
+            max_width, max_height = 500, 500
+            width, height = img.size
+            scale = min(max_width / width, max_height / height)
+            new_size = (int(width * scale), int(height * scale))
+            resized_img = img.resize(new_size)
+
+            new_name = i.replace("_Full_Wish.webp", "_Resize_Wish_500_500.webp")
+            resized_img.save("static/img/characters/" + new_name)
+
+    print("resize is done")
+
+
+def resize_main():
+    img = Image.open("static/img/gi_index_original.webp")
+    resized_img = img.resize((1920, 1080))
+    resized_img.save("static/img/gi_index.webp")
+    print("resize_main is done")
+
+
+resize_main()
+# prepare_ch_json()
+print("all done")
--- load.py
+++ load.py
@@ -1,62 +1,39 @@
+import json
 import os
 
 from jinja2 import Environment, FileSystemLoader
 
-# import model
-
 TEMPLATE_PATH = "templates"
-INDEX_NAME = "index.html"
 BUILD_PATH = "build"
-ELEMENTS = {
-    "Анемо": "/static/img/element/Anemo.svg",
-    "Гео": "/static/img/element/Geo.svg",
-    "электро": "/static/img/element/Electro.svg",
-    "Дэндро": "/static/img/element/Dendro.svg",
-    "Гидро": "/static/img/element/Hydro.svg",
-    "Пиро": "/static/img/element/Pyro.svg",
-    "Крио": "/static/img/element/Cryo.svg"
-}
-SOCIAL = {
-    "YouTube": {
-        "src": "/static/img/social/youtube.svg",
-        "href": "https://www.youtube.com/"
-    },
-    "Twitch": {
-        "src": "/static/img/social/twitch.svg",
-        "href": "https://www.twitch.tv/"
-    },
-    "VK": {
-        "src": "/static/img/social/vk.svg",
-        "href": "https://vk.com/"
-    },
-    "Spotify": {
-        "src": "/static/img/social/spotify.svg",
-        "href": "https://open.spotify.com/"
-    },
+DATA_PATH = "data"
 
-    "Discord": {
-        "src": "/static/img/social/discord.svg",
-        "href": "https://discord.com/"
-    },
-    "Instagram": {
-        "src": "/static/img/social/instagram.svg",
-        "href": "https://instagram.com/"
-    },
-    "HoYoLAB": {
-        "src": "/static/img/social/hoyolab.png",
-        "href": "https://www.hoyolab.com/"
-    },
-    "X": {
-        "src": "/static/img/social/twitter.svg",
-        "href": "https://twitter.com/"
-    },
-}
+INDEX_NAME = "index.html"
+ELEMENTS_NAME = "elements.json"
+SOCIAL_NAME = "social.json"
+CHARACTERS = "characters.json"
 
 
 def build(win_stat: dict):
+    with open(os.path.join(DATA_PATH, ELEMENTS_NAME)) as f:
+        elements = json.load(f)
+    with open(os.path.join(DATA_PATH, SOCIAL_NAME)) as f:
+        social = json.load(f)
+    with open(os.path.join(DATA_PATH, CHARACTERS)) as f:
+        characters = json.load(f)
+
     os.makedirs(BUILD_PATH, exist_ok=True)
     jinja = Environment(loader=FileSystemLoader(TEMPLATE_PATH))
-    content = jinja.get_template(INDEX_NAME).render(win_stat=win_stat, elements=ELEMENTS, social=SOCIAL)
+
+    characters = sorted(characters.items(), key=lambda x: x[1]["element"])
+    characters = {k: dict(sorted(v.items())) for k, v in characters}
+
+    render_data = {
+        "win_stat": win_stat,
+        "elements": elements,
+        "social": social,
+        "characters": characters
+    }
+    content = jinja.get_template(INDEX_NAME).render(**render_data)
 
     with open(os.path.join(BUILD_PATH, INDEX_NAME), "w", encoding="utf-8") as f:
         f.write(content)
